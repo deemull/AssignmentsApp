f7cb77c381728170d37cfa6abd6c89ef63ce76f5
Derrick Mullins <deemullins22@gmail.com>
2018-02-26 17:58:04 -0600

    Updated to initialize the birthdate in a LocalDateTime object.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+        // Output the current date-time
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(today);
+        //Output tomorrow's date using a formatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        String formatDateTime = today.format(formatter);
+        System.out.println(formatDateTime);
+        //Add 5 weeks to today's LocalDateTime
+        System.out.println(today.plusWeeks(5));
+        //Initialize a LocalDateTime object to your birthdate and the time 12:35AM
+        DateTimeFormatter birthDate = DateTimeFormatter.ofPattern("10-02-1996 12:35:00 P.M.");
+        //System.out.println(birthDate.);
     }
+
 }
