02c43946a0495bfb1a754427e68898f05b7c5c9e
Derrick Mullins <deemullins22@gmail.com>
2018-02-28 10:40:18 -0600

    Updated to output number of days alive.

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,15 @@ public class Main {
         //Add 5 weeks to today's LocalDateTime
         System.out.println(today.plusWeeks(5));
         //Initialize a LocalDateTime object to your birthdate and the time 12:35AM
-        DateTimeFormatter birthDate = DateTimeFormatter.ofPattern("10-02-1996 12:35:00 P.M.");
-        //System.out.println(birthDate.);
+        LocalDateTime birthDate = LocalDateTime.of(1996, 10, 2,12, 35);
+        //Output the day of the week (Sunday - Saturday) that you were born
+        System.out.println(birthDate.getDayOfWeek());
+        //Output the number of days you were alive
+        int years = (int) birthDate.toLocalDate().toEpochDay();
+        int days = (int) (today.toLocalDate().toEpochDay() - years);
+        System.out.println(days);
+        //Output the number of days between two dates
+        
     }
 
 }
