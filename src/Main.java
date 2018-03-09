fda0a725669e95830162e6a523db6cae387ac3cb
Derrick Mullins <deemullins22@gmail.com>
2018-03-08 23:09:24 -0600

    Updated to output sorted dates.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -38,18 +39,108 @@ public class Main {
             System.out.println(date2);
         }
         //create a file with 100 random "month/day/year hour:minutes on each line
-        //see Dates.txt
+        readFile();
+        //Random rand = new Random();
+        //DateTimeFormatter formatFile = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+        ArrayList<LocalDateTime> dates = new ArrayList<>();
+        //File readFile = new File("dates.txt");
+        Scanner fileScan = new Scanner(System.in);
+
         //store the data into an Arraylist of LocalDateTime objects
-        Random rand = new Random();
-        ArrayList<String> dates = new ArrayList<>();
-        File readFile = new File("Dates");
-        Scanner fileScan = new Scanner(readFile);
-        while (fileScan.hasNextLine()) {
-            String date = fileScan.next();
-            dates.add(date);
-        }
+        ArrayList<LocalDateTime> storeDates = storeData();
+        System.out.println(storeDates);
+
+        //Output the number of stored dates in the year [Y]
+        System.out.println("Year? ");
+        int year = fileScan.nextInt();
+        ArrayList<LocalDateTime> numStoredDates = printDate(dates, year);
+        System.out.println(numStoredDates);
+
+        //Count the number of stored dates in the current year
+        int numDates = countNumDates(dates);
+
+        //Count the number of duplicates
+
+        //Sort the dates in chronological order
+        Collections.sort(dates);
         System.out.println(dates);
-        //Output the number of stored dates in a year
+        //Count the number of evening (after6pm) dates
+
+        //
+
     }
+
+    public static void readFile() {
+        File readFile = new File("dates.txt");
+        try (PrintWriter printWriter = new PrintWriter(readFile)) {
+            LocalDateTime today = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM:dd:yy HH:mm");
+            Random randNum = new Random();
+            int randDate = randNum.nextInt(100);
+            int num = 100;
+            for (int i = 0; i < num; i++) {
+                LocalDateTime day = today.plusDays(randDate);
+                LocalDateTime hour = today.plusHours(randDate).plusMinutes(randDate);
+                String formatDay = day.format(formatter);
+                String formatHour = hour.format(formatter);
+                printWriter.println(formatDay);
+                printWriter.println(formatHour);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static ArrayList<LocalDateTime> storeData() {
+        File infile = new File("dates.txt");
+        ArrayList<LocalDateTime> dates = new ArrayList<>();
+        try (Scanner newScan = new Scanner(infile)) {
+            while (newScan.hasNext()) {
+                String dateString = newScan.nextLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+                LocalDateTime date = LocalDateTime.parse(dateString, formatter);
+                dates.add(date);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return dates;
+    }
+
+    public static ArrayList<LocalDateTime> printDate(ArrayList<LocalDateTime> dates, int year) {
+        ArrayList<LocalDateTime> specYear = new ArrayList<>();
+        for (int i = 0; i < dates.size(); i++) {
+            if (dates.get(i).getYear() == year) {
+                specYear.add(dates.get(i));
+            }
+        }
+        return specYear;
+
+    }
+
+    public static int countNumDates(ArrayList<LocalDateTime> dates) {
+        LocalDateTime today = LocalDateTime.now();
+        int count = 0;
+        for (int i = 0; i < dates.size(); i++) {
+            if (dates.get(i).getYear() == today.getYear()) {
+                count++;
+            }
+        }
+        return count;
+    }
+    /*public static int countDuplicates(ArrayList<LocalDateTime> dates) {
+        int count = 0;
+        Set<LocalDateTime> date = new HashSet<>();
+        for (int i = 0; i < dates.size(); i++) {
+            if (dates.get(i) == dates.get(i+1)) {
+                count++;
+            }
+        }
+
+    }
+    */
 
 }
